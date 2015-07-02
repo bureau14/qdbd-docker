d715fc25dda270fe8dc7de3c55a18d5d51268924
Leon Mergen <leon@solatis.com>
2015-07-02 11:37:09 +0700

    Now detects potential license file in qdb data dir.

--- qdbd-docker-wrapper.sh
+++ qdbd-docker-wrapper.sh
@@ -7,4 +7,17 @@ PORT=2836
 
 IP=`${IP} route get 8.8.8.8 | ${AWK} 'NR==1 {print $NF}'`
 
-${QDB_SERVER} -a ${IP}:${PORT} $@
+
+# Detects the presence of a license file, and provides it as an
+# argument to `qdbd` if found.
+POTENTIAL_LICENSE_FILE="/var/lib/qdb/license.txt"
+LICENSE_FILE_PARAMETER=""
+
+if [ -f ${POTENTIAL_LICENSE_FILE} ]; then
+    echo "license file found! ${POTENTIAL_LICENSE_FILE}"
+    LICENSE_FILE_PARAMETER="--license-file ${POTENTIAL_LICENSE_FILE}"
+fi
+
+echo "parameter = ${LICENSE_FILE_PARAMETER}"
+
+${QDB_SERVER} ${LICENSE_FILE_PARAMETER} -a ${IP}:${PORT} $@
